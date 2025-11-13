687c2b63f6acb77ccc7e2704df887c742a0d0ac0
theprojectctrl <contact.projectctrl@gmail.com>
2025-11-12 19:17:03 -0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+      apiKey: "AIzaSyDMEwjbVeqk5BZftkfz8fh0-8105FZYHkI",
+      authDomain: "qrtists-5ba2e.firebaseapp.com",
+      projectId: "qrtists-5ba2e",
+      storageBucket: "qrtists-5ba2e.firebasestorage.app",
+      messagingSenderId: "915309919101",
+      appId: "1:915309919101:web:efbb24192ea874a6a06a14",
+  };
+  
